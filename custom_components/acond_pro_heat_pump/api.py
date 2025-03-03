38cdb9ed5a903474426d69928615a4c43734793e
Piotrek <piotr.lyczko@beneering.com>
2025-03-03 12:40:31 +0100

    set aut content type

--- custom_components/acond_pro_heat_pump/api.py
+++ custom_components/acond_pro_heat_pump/api.py
@@ -148,12 +148,12 @@ class AcondProApiClient:
 
                 ) 
                 # if response.headers.location == URL_LOGIN:
-                    data = FormData(quote_fields=True, charset='utf-8')
-                    content_type= 'application/x-www-form-urlencoded'
-                    data.add_field('USER', self._username, content_type='application/x-www-form-urlencoded')
-                    data.add_field('PASS', self._password, content_type='application/x-www-form-urlencoded')
-                    response = await self._session.request(
+                data = FormData(quote_fields=True, charset='utf-8')
+                data.add_field('USER', self._username, content_type='application/x-www-form-urlencoded')
+                data.add_field('PASS', self._password, content_type='application/x-www-form-urlencoded')
+                response = await self._session.request(
                     method='post',
+                    content_type= 'application/x-www-form-urlencoded',
                     url="https://" + self._ip_address + URL_LOGIN,
                     headers=headers,
                     data=data,
