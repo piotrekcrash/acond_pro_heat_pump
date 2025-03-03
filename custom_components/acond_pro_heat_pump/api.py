758795cedef3d8ef85f5a7a4a90ebb5718718751
Piotrek <piotr.lyczko@beneering.com>
2025-03-03 13:01:43 +0100

    test

--- custom_components/acond_pro_heat_pump/api.py
+++ custom_components/acond_pro_heat_pump/api.py
@@ -155,10 +155,10 @@ class AcondProApiClient:
                     headers=headers,
                     json=data,
                 )
-
+                #  content_type='application/x-www-form-urlencoded'
                 data = FormData(quote_fields=True, charset='utf-8')
-                data.add_field('USER', self._username, content_type='application/x-www-form-urlencoded')
-                data.add_field('PASS', self._password, content_type='application/x-www-form-urlencoded')
+                data.add_field('USER', self._username)
+                data.add_field('PASS', self._password)
                 response = await self._session.request(
                     method='post',
 
