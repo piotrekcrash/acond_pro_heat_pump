5b76404d1e4795f119fb9d2bc35ec8ac979c5f54
Piotrek <piotr.lyczko@beneering.com>
2025-03-05 12:39:42 +0100

    api level session

--- custom_components/acond_pro_heat_pump/api.py
+++ custom_components/acond_pro_heat_pump/api.py
@@ -54,8 +54,8 @@ class AcondProApiClient:
         self._ip_address = ip_address
         self._username = username
         self._password = password
-        self._session = session
-        # self._session = aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True), connector=aiohttp.TCPConnector(ssl=ssl_context))
+        # self._session = session
+        self._session = aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True), connector=aiohttp.TCPConnector(ssl=ssl_context))
 
     async def async_get_data(self) -> Any:
         """Get data from the API."""
@@ -151,10 +151,10 @@ class AcondProApiClient:
         """Get information from the API."""
         try:
             async with async_timeout.timeout(10):
-                sess = aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True), connector=aiohttp.TCPConnector(ssl=ssl_context))
+                # sess = aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True), connector=aiohttp.TCPConnector(ssl=ssl_context))
                 # cookie_jar = aiohttp.CookieJar(unsafe=True)
                 # async with aiohttp.ClientSession(cookie_jar=cookie_jar, connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
-                async with sess as session:
+                async with self._session as session:
                     await session.get("https://" + self._ip_address + URL_LOGIN)
                     response = await session.post(url="https://" + self._ip_address + URL_LOGIN, data=self.login_form())
                     body = await response.read()
