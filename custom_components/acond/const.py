6b2851796a926857463db20f546eff8eff482756
Piotrek <piotr.lyczko@beneering.com>
2025-12-09 14:25:11 +0100

    update domain

--- custom_components/acond/const.py
+++ custom_components/acond/const.py
@@ -4,7 +4,7 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "acond_pro_heat_pump"
+DOMAIN = "acond"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
 
 URL_LOGIN = "/SYSWWW/LOGIN.XML"
