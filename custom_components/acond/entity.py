1c3544712ebb738f453470867722060b8c574406
Piotrek <piotr.lyczko@beneering.com>
2025-12-10 15:35:06 +0100

    chnage identifiers

--- custom_components/acond/entity.py
+++ custom_components/acond/entity.py
@@ -23,7 +23,8 @@ class AcondProEntity(CoordinatorEntity[AcondDataUpdateCoordinator]):
             identifiers={
                 (
                     coordinator.config_entry.domain,
-                    coordinator.config_entry.entry_id,
+                    #coordinator.config_entry.entry_id,
+                    "MAC_ADDRES_OF_DEVICE"
                 ),
             },
             default_name="Heat Pump",
